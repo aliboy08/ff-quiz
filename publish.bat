ea7350e7289247268a3bec6a51f89978ecbc790e
Alistair Ponce <alistair.ponce@gmail.com>
2024-07-30 15:51:42 +0900

    boilerplate update

--- publish.bat
+++ publish.bat
@@ -5,7 +5,7 @@ set "plugin_name=plugin-boilerplate"
 set "zip_file=%plugin_name%.zip"
 
 set "source_path=%CD%"
-set "exclude=node_modules;.git;_temp;zip.ps1;publish.bat;.gitignore;jsconfig.json;package.json;package-lock.json;wp-manifest.cjs;vite.config.js;%zip_file%"
+set "exclude=node_modules;.git;zip.ps1;publish.bat;.gitignore;jsconfig.json;package.json;package-lock.json;wp-manifest.cjs;vite.config.js;%zip_file%"
 
 echo Creating zip file...
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0zip.ps1" -source_path "%source_path%" -plugin_name "%plugin_name%" -exclude "%exclude%"

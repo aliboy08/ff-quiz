87491b70114b6916607e1dbe2f7600a863db8ec9
Alistair Ponce <alistair.ponce@gmail.com>
2024-07-30 15:52:47 +0900

    boilerplate update

--- publish.bat
+++ publish.bat
@@ -10,10 +10,11 @@ set "exclude=node_modules;.git;zip.ps1;publish.bat;.gitignore;jsconfig.json;pack
 echo Creating zip file...
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0zip.ps1" -source_path "%source_path%" -plugin_name "%plugin_name%" -exclude "%exclude%"
 
-echo Uploading to remote(wpengine)...
 set "install_name=devlibrary2021"
 set "remote_path=/sites/%install_name%/wp-content/plugins/fivebyfive/modules/"
 set "ssh_host=%install_name%@%install_name%.ssh.wpengine.net"
+
+echo Uploading to remote(%install_name%)...
 
 scp %zip_file% %ssh_host%:%remote_path%
 

41bbe947a7b20320bbab0a9c5ac10258efa2a0de
Alistair Ponce <alistair.ponce@gmail.com>
2024-08-01 15:36:47 +0900

    Update admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -1,8 +1,12 @@
 <?php
 namespace FFPlugin\PluginName;
 
-add_action('admin_menu', function(){
-    add_submenu_page( 'fivebyfive', PLUGIN_NAME, PLUGIN_NAME, 'manage_options', PLUGIN_SLUG, function(){
-        include 'settings.php';
+add_action('plugins_loaded', function(){
+
+    add_action('admin_menu', function(){
+        add_submenu_page( 'fivebyfive', PLUGIN_NAME, PLUGIN_NAME, 'manage_options', PLUGIN_SLUG, function(){
+            include 'settings.php';
+        });
     });
+
 });
